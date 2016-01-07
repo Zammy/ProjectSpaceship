003aad9980203acc9c30b290cc2febee566b902d
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-01-07 19:07:21 +0200

    Got some simple ship prototype to work. Still not sure if this is going to be fun.

---
+++ client/Spaceship Command/Assets/Game/InputScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputScript : MonoBehaviour 
+{
+
+    //Set through Unity
+//    public Thruster FrontLeft;
+//    public Thruster FrontRight;
+    public Thruster Front;
+    public Thruster BackLeft;
+    public Thruster BackRight;
+    //
+
+	
+    void Update()
+    {
+//        this.FrontLeft.IsActive = Input.GetKey(KeyCode.Q);
+//        this.FrontRight.IsActive = Input.GetKey(KeyCode.E);
+        this.Front.IsActive = Input.GetKey(KeyCode.W);
+        this.BackLeft.IsActive = Input.GetKey(KeyCode.A);
+        this.BackRight.IsActive = Input.GetKey(KeyCode.D);
+
+//        if (this.Thrusters.Length == 0)
+//        {
+//            return;
+//        }
+//
+//        this.Thrusters[0].IsActive = Input.GetKey(KeyCode.Q);
+//
+//        if (this.Thrusters.Length == 1)
+//        {
+//            return;
+//        }
+//
+//        this.Thrusters[1].IsActive = Input.GetKey(KeyCode.W);
+    }
+}
---
+++ client/Spaceship Command/Assets/Game/Thruster.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Thruster : MonoBehaviour 
+{
+    //Set through Unity
+    public float POWER;
+
+    public Rigidbody2D ApplyForceOn;
+
+    public ParticleSystem Particles;
+    //
+
+    private bool isActive = false;
+    public bool IsActive
+    {
+        get
+        {
+            return this.isActive;
+        }
+
+        set
+        {
+            if (this.isActive == value)
+                return;
+
+            this.isActive = value;
+
+            this.Particles.gameObject.SetActive(value);
+        }
+
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log(this.transform.forward);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    if (this.isActive )
+        {
+            var forward = this.transform.up * POWER;
+            this.ApplyForceOn.AddForce(forward);
+        }
+	}
+}
