df2a70945a76963307809e615d30ec7377b83635
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-02-08 21:15:19 +0200

    Dynamic 2D camera done!

---
+++ client/Spaceship Command/Assets/Game/FocusCamera.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FocusCamera : MonoBehaviour 
+{
+    public GameObject[] FocusOn;
+
+    Camera camera;
+
+    float targetOrtho;
+    Vector3 targetPos;
+
+
+    const float BUFFER = 1;
+    const float orthoMovePerSec = 10f;
+    const float posMovePerSec = 30f;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        this.camera = this.GetComponent<Camera>();
+
+        this.CalculateCameraSizeAndPos();
+	}   
+
+	void Update () 
+    {
+        this.CalculateCameraSizeAndPos();
+
+        if (Mathf.Abs(this.camera.orthographicSize - this.targetOrtho) > 0.1f)
+        {
+            float orthoMoveFrame = Time.deltaTime * orthoMovePerSec;
+            float orthoDelta = this.targetOrtho - this.camera.orthographicSize;
+            if (Mathf.Abs(orthoDelta) < orthoMoveFrame)
+            {
+                orthoMoveFrame = orthoDelta;
+            }
+
+            if (orthoDelta > 0)
+            {
+                this.camera.orthographicSize += orthoMoveFrame;
+            }
+            else
+            {
+                this.camera.orthographicSize -= orthoMoveFrame;
+            }
+        }
+
+        float distanceToMove = posMovePerSec * Time.deltaTime;
+        Vector3 diff = this.targetPos - this.transform.position;
+        if (diff.magnitude > 0.025f)
+        {
+            if (diff.magnitude < distanceToMove)
+            {
+                distanceToMove = diff.magnitude;
+            }
+
+            diff.Normalize();
+            diff = diff * distanceToMove;
+            this.transform.position += diff;
+        }
+	}   
+
+    void CalculateCameraSizeAndPos()
+    {
+        float minHeight;
+        float mediumY;
+        float minWidth;
+        float mediumX;
+        CalculateMinHeight( FocusOn, out minHeight, out mediumY );
+        CalculateMinWidth( FocusOn, out minWidth, out mediumX );
+
+        float calcHeight = minHeight + BUFFER;
+        float calcWidth = calcHeight * this.camera.aspect;
+        if (calcWidth < minWidth)
+        {
+            calcWidth = minWidth;
+            calcHeight = calcWidth / this.camera.aspect;
+        }
+
+        this.targetOrtho = calcHeight / 2;
+        this.targetPos = new Vector3(mediumX, mediumY, this.camera.transform.position.z);
+    }
+
+    void CalculateMinWidth(GameObject[] gos, out float minWidth, out float mediumX)
+    {
+        float minX = float.MaxValue;
+        float maxX = float.MinValue;
+
+        foreach(var go in gos)
+        {
+            float x = GetX(go, min: true);
+            if (minX > x)
+            {
+                minX = x;
+            }
+            x = GetX(go, min: false);
+            if (maxX < x)
+            {
+                maxX = x;
+            }
+        }
+
+        minWidth = maxX - minX;
+        mediumX = minX + minWidth / 2;
+    }
+
+    void CalculateMinHeight(GameObject[] gos, out float minHeight, out float mediumY)
+    {
+        float minY = float.MaxValue;
+        float maxY = float.MinValue;
+
+        foreach (var go in gos)
+        {
+            float y = GetY(go, min:true);
+            if (minY > y)
+            {
+                minY = y;
+            }
+            y = GetY(go, min:false);
+            if (maxY < y)
+            {
+                maxY = y;
+            }
+        }
+
+        minHeight = maxY - minY;
+        mediumY = minY + minHeight / 2;
+    }
+
+    float GetX(GameObject go, bool min)
+    {
+        var sprite = go.GetComponent<SpriteRenderer>().sprite;
+        float width = sprite.rect.width / sprite.pixelsPerUnit;
+        width = width * go.transform.localScale.x;
+        if (min) 
+        {
+            return go.transform.position.x - width/2;
+        }
+        else
+        {
+            return go.transform.position.x + width/2;
+        }
+    }
+
+    float GetY(GameObject go, bool min)
+    {
+        var sprite = go.GetComponent<SpriteRenderer>().sprite;
+        float height = sprite.rect.height / sprite.pixelsPerUnit;
+        height = height * go.transform.localScale.y;
+        if (min) 
+        {
+            return go.transform.position.y - height/2;
+        }
+        else
+        {
+            return go.transform.position.y + height/2;
+        }
+    }
+}
